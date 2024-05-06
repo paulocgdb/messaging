cde882b3661b4e192a3e6ac949141a1b23d3998e
paulo.brito <paulocesarti01@gmail.com>
2024-05-06 20:43:51 -0300

    config: Initial config for the project

---
+++ rabbitmq-testing/src/main/java/com/example/rabbitmqtesting/RabbitmqTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqTestingApplication.class, args);
+	}
+
+}
---
+++ rabbitmq-testing/src/test/java/com/example/rabbitmqtesting/RabbitmqTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
