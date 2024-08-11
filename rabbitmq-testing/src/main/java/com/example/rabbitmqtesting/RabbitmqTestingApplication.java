1ca8da4bb66400e36bb1169b52998fcc70c4ce42
Paulo César <paulocesarti01@gmail.com>
2024-08-11 19:46:17 -0300

    Update RabbitmqTestingApplication.java

--- rabbitmq-testing/src/main/java/com/example/rabbitmqtesting/RabbitmqTestingApplication.java
+++ rabbitmq-testing/src/main/java/com/example/rabbitmqtesting/RabbitmqTestingApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RabbitmqTestingApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(RabbitmqTestingApplication.class, args);
+		System.out.println("Working");
 	}
 
 }
